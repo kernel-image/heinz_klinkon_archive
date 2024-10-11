2dfab8235955d23ff7ca22092dcf2b7935eb46f9
Peter Klinkon <info@kernel-image.net>
2024-10-11 15:25:12 -0400

    define AppContext

---
+++ client/src/utils/AppContext.js
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+export const AppContext = createContext({
+    results: [],
+    error: null,
+    searching: false,
+    selectedResult: null,
+    setSelectedResult: () => {},
+    search: "",
+    setSearch: () => {},
+    selectedClass: false,
+    setSelectedClass: () => {},
+  });
