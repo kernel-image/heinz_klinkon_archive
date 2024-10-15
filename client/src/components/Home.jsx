f074c82d39018c49ba7c5a3fa334fe70f79729d1
Peter Klinkon <info@kernel-image.net>
2024-10-15 14:01:10 -0400

    wrap home content in content-wrapper div like archive item component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,10 +6,10 @@ function Home() {
     //console.log("rendering default content");
     const homeimg = "HKLW0096"
     return (
-        <>
+        <div className="content-wrapper"> 
         <ImageContainer id = {homeimg} />
         <WelcomeMessage message = {`HEINZ KLINKON (1941-2008) was an artist, designer, and educator.\nThis website attempts to catalogue his work for interested parties to browse.`}/>
-        </>
+        </div>
     )
 }
 export default Home
