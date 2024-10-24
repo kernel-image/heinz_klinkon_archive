e80956b4b7af8a23851e4136c4d8a2c2ecf05962
Peter Klinkon <info@kernel-image.net>
2024-10-23 20:33:22 -0400

    update home page image id and add email link

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,11 +4,11 @@ import WelcomeMessage from './WelcomeMessage'
 
 function Home() {
     //console.log("rendering default content");
-    const homeimg = "HKLW0096"
+    const homeimg = "hk0120"
     return (
         <> 
         <ImageContainer id = {homeimg} />
-        <WelcomeMessage message = {`HEINZ KLINKON (1941-2008) was an artist, designer, and educator.\nThis website attempts to catalogue his work for interested parties to browse.`}/>
+        <WelcomeMessage message = {`HEINZ KLINKON (1941-2008) was an artist, designer, and educator.\nThis website attempts to catalogue his work for interested parties to browse.\nAdditional images and information will be added on an ongoing basis.\n<a href="mailto:info@kernel-image.net">Email</a> with inquiries.`}/>
         </>
     )
 }
