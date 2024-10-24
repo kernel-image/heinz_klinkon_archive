5c9d8e119618cd1e243671df90d107e340afdf75
Peter Klinkon <info@kernel-image.net>
2024-10-24 16:43:52 -0400

    modified useResults hook to accept ReadableStream response body

--- client/src/utils/useResults.js
+++ client/src/utils/useResults.js
@@ -6,6 +6,21 @@ const getURLforSearch = (search, port=5173) => {
     return baseURL + "?search=" + encodeURIComponent(search);
 }
 
+const readStream = async (stream) => {
+    const reader = stream.getReader();
+    const decoder = new TextDecoder();
+    let done = false;
+    let result = "";
+    while (!done) {
+        const { value, done: doneReading } = await reader.read();
+        done = doneReading;
+        const chunkValue = decoder.decode(value);
+        result += chunkValue;
+    }
+    console.log(result);
+    return result;
+}
+
 export function useResults (search) {
     const [results, setResults] = useState([]);
     const [error, setError] = useState(null);
@@ -31,19 +46,25 @@ export function useResults (search) {
                     }
                 })
                 .then((response) => {
-                    
                     if (response.status >= 400) {
                         setError("server error");
                         setResults([]);
                         return;
                         //throw new Error("server error");
                     }
+                    if (response.status === 200 && response.body instanceof ReadableStream) {
+                        //console.log("response is stream");
+                        return readStream(response.body);
+                    }
                     const data = response.json();
                     return data;
                 })
                 .then((data) => {
-                    
-                    setResults(data.results);
+                    if (typeof data === "string") {
+                        setResults(JSON.parse(data));
+                    }else{
+                        setResults(data.results);
+                    }
                     setError(null);
                 })
                 .catch(error => setError(error))
