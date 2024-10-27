fd0d69158087c6764b4b2a62760fc4eb5e906eb9
Peter Klinkon <info@kernel-image.net>
2024-10-26 21:32:13 -0400

    update env var names to match vercel

--- server/config/config.js
+++ server/config/config.js
@@ -1,25 +1,25 @@
 import 'dotenv/config'
 
 const validateEnv = () => {
-    if (!process.env.PG_HOST) {
-        throw new Error('PG_HOST is not defined');
+    if (!process.env.POSTGRES_HOST) {
+        throw new Error('POSTGRES_HOST is not defined');
     }
     if (!process.env.PG_PORT) {
         throw new Error('PG_PORT is not defined');
     }
-    if (!process.env.PG_USER) {
-        throw new Error('PG_USER is not defined');
+    if (!process.env.POSTGRES_USER) {
+        throw new Error('POSTGRES_USER is not defined');
     }
-    if (!process.env.PG_PASSWORD) {
-        throw new Error('PG_PASSWORD is not defined');
+    if (!process.env.POSTGRES_PASSWORD) {
+        throw new Error('POSTGRES_PASSWORD is not defined');
     }
-    if (!process.env.PG_DATABASE) {
-        throw new Error('PG_DATABASE is not defined');
+    if (!process.env.POSTGRES_DATABASE) {
+        throw new Error('POSTGRES_DATABASE is not defined');
     }
     console.log("env validated")
     /*
     for (const key in process.env) {
-        if (key.startsWith('PG_')) {
+        if (key.startsWith('POSTGRES_')) {
             console.log(`${key} = ${process.env[key]}`);
         }
     }
@@ -30,11 +30,11 @@ validateEnv()
 
 function getConfig() {
     return {
-        host: process.env.PG_HOST,
+        host: process.env.POSTGRES_HOST,
         port: process.env.PG_PORT,
-        user: process.env.PG_USER,
-        password: process.env.PG_PASSWORD,
-        database: process.env.PG_DATABASE,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
         validate: function () {
             for (const key in this) {
                 if (key !== 'validate')
