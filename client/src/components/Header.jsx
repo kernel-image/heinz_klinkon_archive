fa0c59ac58202130399da128afa688cbbc5cb226
Peter Klinkon <info@kernel-image.net>
2024-11-04 11:50:11 -0500

    reset search results when clicking on header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,12 @@
 import prezel from '../icons/PREZEL.svg'
 import '../styles/Header.css'
+import { useContext } from 'react'
+import { AppContext } from '../utils/AppContext'
 
 function Header() {
+    const {setSearch} = useContext(AppContext);
     return (
-        <div className = "header">
+        <div className = "header" onClick={() => setSearch("hk")}>
             <h1>HEINZ KLINKON</h1>
             <div className = "sub-header">
                 <h2>ART</h2><img src={prezel} className = "prezel" alt={"\u0026"} /><h2>DESIGN</h2>
